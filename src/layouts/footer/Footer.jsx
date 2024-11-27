af61588c3b4990bbdd6a075236a6d107ca825b7e
StaglianoM <stagliano.m92@gmail.com>
2024-11-27 20:24:16 +0100

    add footer

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -1,3 +1,11 @@
+import './Footer.css'
+
 export default function Footer() {
-    return (<footer>Footer</footer>);
+    return (
+        <div className="footer-container">
+            <div className="footer-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium provident sint sequi iste sit neque dolore amet. Ratione, deserunt ab?</p>
+            </div>
+        </div>
+    );
 }
